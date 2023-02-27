af4397e9e75840f19602bc2d75d1f79072131897
sarmadkamal464 <sarmadkamal464@gmail.com>
2023-02-27 12:03:33 +0500

    [blade] link removes

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -54,8 +54,7 @@
                                                 <label for="wt-jobs">Projects</label>
                                             </span>
                                         </div>
-                                        <a href="userlisting.html" class="wt-searchbtn"><i
-                                                class="lnr lnr-magnifier"></i></a>
+                                        <a class="wt-searchbtn"><i class="lnr lnr-magnifier"></i></a>
                                     </div>
                                 </div>
                             </fieldset>
