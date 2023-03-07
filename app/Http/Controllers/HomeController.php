cc6efcbb04a382ae46651a3928a1a21679d7487a
sarmadkamal464 <sarmadkamal464@gmail.com>
2023-03-07 18:18:35 +0500

    [MVC] remove categoryAPI class

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,19 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Controllers\Api\CategoryApiController;
 use App\Models\Category;
 use App\Models\Country;
 
 class HomeController extends Controller
 {
-    protected $category;
-
-    public function __construct(CategoryApiController $category)
-    {
-        $this->category = $category;
-    }
-
     public function index()
     {
         return view('site.home');
