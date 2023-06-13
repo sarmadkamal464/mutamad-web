7540843d8b8c7e227ca2a7cbbd3dce6cae4bf0ff
sarmad <sarmadkamal464@gmail.com>
2023-06-13 10:28:55 +0500

    added StripePaymentController

--- app/Http/Controllers/StripePaymentController.php
+++ app/Http/Controllers/StripePaymentController.php
@@ -8,29 +8,44 @@ use Stripe\Customer;
 
 class StripePaymentController extends Controller
 {
-   public function createCustomer(Request $request)
-{
-    // Set your Stripe secret key
-    Stripe::setApiKey(config('services.stripe.secret'));
+    /**
+     * Display the paymentStripe view.
+     *
+     * @return \Illuminate\Contracts\View\View
+     */
+    public function paymentStripe()
+    {
+        return view('site.paymentStripe');
+    }
 
-    // Extract customer data from the request
-    $name = $request->input('name');
-    $email = $request->input('email');
-    $stripeToken = $request->input('stripeToken'); // Assuming you're receiving the token from the frontend
+    /**
+     * Create a new customer in Stripe.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function createCustomer(Request $request)
+    {
+        // Set your Stripe secret key
+      Stripe\Stripe::setApiKey(env('STRIPE_SECRET'));
+       
+        // Extract customer data from the request
+        $name = $request->input('name');
+        $email = $request->input('email');
+        $stripeToken = $request->input('stripeToken'); // Assuming you're receiving the token from the frontend
 
-    // Create a new customer in Stripe
-    $customer = Customer::create([
-        'name' => $name,
-        'email' => $email,
-        'source' => $stripeToken,
-        
-    ]);
+        // Create a new customer in Stripe
+        $customer = Customer::create([
+            'name' => $name,
+            'email' => $email,
+            'source' => $stripeToken,
+        ]);
 
-    // Handle the customer creation response
-    // For example, you can store the customer ID in your database
-    $customerId = $customer->id;
+        // Handle the customer creation response
+        // For example, you can store the customer ID in your database
+        $customerId = $customer->id;
 
-    // Return a response indicating success or failure
-    return response()->json(['message' => 'Customer created successfully', 'customerId' => $customerId]);
-}
+        // Return a response indicating success or failure
+        return response()->json(['message' => 'Customer created successfully', 'customerId' => $customerId]);
+    }
 }
