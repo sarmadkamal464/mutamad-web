028c530485bc948218aed435c97c4bf00d013dce
sarmad <sarmadkamal464@gmail.com>
2023-06-21 10:39:15 +0500

    AddFreelancer Account on stripe function

--- app/Http/Controllers/StripePaymentController.php
+++ app/Http/Controllers/StripePaymentController.php
@@ -51,4 +51,26 @@ class StripePaymentController extends Controller
         // Return a response indicating success or failure
         return response()->json(['message' => 'Customer created successfully', 'customerId' => $customerId]);
     }
+
+    public function addFreelancerAccount(Request $request)
+    {
+      $name = $request->input('name');
+        $email = $request->input('email');
+        $stripeToken = $request->input('stripeToken'); // Assuming you're receiving the token from the frontend
+
+        // Create a new customer in Stripe
+        $customer = Customer::create([
+            'name' => $name,
+            'email' => $email,
+            'source' => $stripeToken,
+        ]);
+
+        // Handle the customer creation response
+        // For example, you can store the customer ID in your database
+        $customerId = $customer->id;
+
+        // Return a response indicating success or failure
+        return response()->json(['message' => 'Customer created successfully', 'customerId' => $customerId]);
+
+    }
 }
