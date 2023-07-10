df6802113e46e63be7866f86ae17d06e6e87ef1f
Sarmad Kamal Khan <89574306+sarmadkamal464@users.noreply.github.com>
2023-07-10 18:05:47 +0500

    Update server.js

--- nodeServer/server.js
+++ nodeServer/server.js
@@ -2,10 +2,9 @@ const app = require('express')();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server, {
   cors: {
-    origin: '*',
+    origin: 'http://127.0.0.1:8000',
     methods: ['GET', 'POST'],
-    allowedHeaders: ['Content-Type'],
-    credentials: true
+  
   }
 });
 
